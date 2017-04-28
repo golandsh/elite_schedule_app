6ecc7097f58da9dd6580be384d4e0830e66cd855
Shlomi Goland <golandsh@gmail.com>
2017-04-28 09:58:38 +0300

    Navigation

--- src/pages/team-home/team-home.page.ts
+++ src/pages/team-home/team-home.page.ts
@@ -1,21 +1,27 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-import { StandingsPage, TeamDetailPage } from '../pages';
+import { MyTeamsPage, StandingsPage, TeamDetailPage } from '../pages';
 
 @Component({
   selector: 'page-team-home',
   templateUrl: 'team-home.page.html',
 })
 export class TeamHomePage {
+  team: any;
   teamDetailTab = TeamDetailPage;
   standingsTab = StandingsPage;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.team = this.navParams.data;
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad TeamHome');
   }
 
+  goHome() {
+    //this.navCtrl.push(MyTeamsPage);
+    this.navCtrl.popToRoot();
+  }
 }
